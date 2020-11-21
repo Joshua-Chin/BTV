7e6a3e7b2f9425678295383a983c8ae7fb51a56a
Joshua Chin <joshuarchin@gmail.com>
2020-11-20 19:36:45 -0500

    Add nonnan for ordered f32

---
+++ src/non_nan.rs
@@ -0,0 +1,22 @@
+use std::cmp::Ordering;
+
+#[derive(PartialEq,PartialOrd)]
+pub struct NonNan(f32);
+
+impl NonNan {
+    pub fn new(val: f32) -> Option<NonNan> {
+        if val.is_nan() {
+            None
+        } else {
+            Some(NonNan(val))
+        }
+    }
+}
+
+impl Eq for NonNan {}
+
+impl Ord for NonNan {
+    fn cmp(&self, other: &NonNan) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
