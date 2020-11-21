8763a5099595756730ce17451096ac540098f442
Joshua Chin <joshuarchin@gmail.com>
2020-11-20 19:37:27 -0500

    Implement solver code

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::ops::Deref;
+
 #[macro_use]
 extern crate bitflags;
 
 mod abilities;
 mod challenges;
 mod distributions;
+mod non_nan;
 mod rewards;
+mod solver;
 
 fn main() {
-    let mut solver = challenges::Solver::new(vec!((5, 4), (2, 3), (1, 6)));
-    println!("{:?}", solver.convex_hull(rewards::Rewards::NONE, 0));
+    let solver = challenges::Solver::new(vec!((5, 4), (2, 3), (1, 6)));
+    let h1 = solver.convex_hull(rewards::Rewards::NONE, 0);
+    let h2 = solver.convex_hull(rewards::Rewards::NONE, 1);
+    let h3 = solver.convex_hull(rewards::Rewards::NONE, 2);
+    let hulls = [h1.deref(), h2.deref(), h3.deref()];
+    println!("{:?}", solver::merge_hulls(&hulls));
 }
---
+++ src/solver.rs
@@ -0,0 +1,55 @@
+use std::collections::BinaryHeap;
+
+use crate::non_nan::NonNan;
+use crate::{abilities::AbilitySet, challenges::HullPoint};
+
+const CHALLENGES: usize = 3;
+
+pub fn merge_hulls<T: AsRef<[HullPoint]> + Sized>(
+    hulls: &[T; CHALLENGES],
+) -> Vec<(u32, f32, [AbilitySet; CHALLENGES])> {
+    // Initialize running variables
+    let mut total_cost = 0;
+    let mut total_log_probability = 0.0;
+    let mut configuration = [AbilitySet::new(); CHALLENGES];
+
+    // Initialize heap
+    let mut heap = BinaryHeap::new();
+    heap.reserve_exact(hulls.len());
+
+    // Iterate through each hull
+    for (idx, hull) in hulls.iter().enumerate() {
+        match hull.as_ref().get(0) {
+            Some((cost, log_probability, _slope, abilities)) => {
+                total_cost += cost;
+                total_log_probability += log_probability;
+                configuration[idx] = *abilities;
+            }
+            None => {
+                return vec![];
+            }
+        }
+        match hull.as_ref().get(1) {
+            Some((_, _, slope, _abilities)) => {
+                heap.push((NonNan::new(*slope).unwrap(), idx, 1));
+            }
+            None => {}
+        }
+    }
+
+    let mut output = vec![];
+    output.push((total_cost, total_log_probability, configuration));
+
+    while let Some((_slope, hull, idx)) = heap.pop() {
+        let h = hulls[hull].as_ref();
+        total_cost += h[idx].0 - h[idx - 1].0;
+        total_log_probability += h[idx].1 - h[idx - 1].1;
+        configuration[hull] = hulls[hull].as_ref()[idx].3;
+        if let Some((_, _, slope, _abilities)) = h.get(idx + 1) {
+            heap.push((NonNan::new(*slope).unwrap(), hull, idx + 1));
+        }
+        output.push((total_cost, total_log_probability, configuration));
+    }
+
+    output
+}
