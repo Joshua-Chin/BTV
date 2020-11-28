1f799a9419caedb2527a175724ce93c279b7deb3
Joshua Chin <joshuarchin@gmail.com>
2020-11-28 18:16:05 -0500

    Implement parser

---
+++ src/parser.rs
@@ -0,0 +1,63 @@
+use crate::rewards::Rewards;
+
+#[derive(Debug, Eq, PartialEq)]
+pub struct Challenge {
+    name: String,
+    target: u32,
+    abilities: u32,
+    reward: Rewards,
+}
+
+pub fn parse<T: AsRef<str>>(text: T) -> Option<Vec<Challenge>> {
+    let mut output = Vec::with_capacity(11);
+
+    for entry in text.as_ref().lines().collect::<Vec<&str>>().chunks_exact(2) {
+        // Parse the first line
+        let mut l1 = entry[0].split('\t');
+        // Parse name
+        let name = l1.next()?.trim().to_string();
+        // Parse target
+        let target: u32 = l1.next()?.trim().strip_prefix("Target: ")?.parse().ok()?;
+        // Parse abilities
+        let abilities: u32 = l1
+            .next()?
+            .trim()
+            .strip_prefix("Max Abilities: ")?
+            .parse()
+            .ok()?;
+        // Parse the reward
+        let reward: Rewards = entry[1].trim().strip_prefix("Reward: ")?.parse().ok()?;
+
+        output.push(Challenge {
+            name,
+            target,
+            abilities,
+            reward,
+        });
+    }
+
+    Some(output)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse() {
+        let input = include_str!("data.txt");
+        let result = parse(input);
+        assert!(result.is_some());
+        let challenges = result.unwrap();
+        assert!(challenges.len() == 11);
+        assert_eq!(
+            challenges[0],
+            Challenge {
+                name: "Monologue".to_string(),
+                target: 70,
+                abilities: 15,
+                reward: Rewards::DICTION_RANGE
+            }
+        );
+    }
+}
--- src/rewards.rs
+++ src/rewards.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 bitflags! {
     pub struct Rewards: u32 {
         const NONE = 0;
@@ -15,5 +17,23 @@ bitflags! {
 impl Rewards {
     pub fn combinations() -> impl Iterator<Item = Rewards> {
         (0..256).map(Rewards::from_bits_truncate)
+    }
+}
+
+impl FromStr for Rewards {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(match s {
+            "+1 Ability max per challenge" => Rewards::ADDITIONAL_ABILITY,
+            "+1 Atmosphere Range" => Rewards::ATMOSPHERE_RANGE,
+            "+1 Diction Strength" => Rewards::DICTION_STRENGTH,
+            "+1 Diction Range" => Rewards::DICTION_RANGE,
+            "+1 Precision Strength" => Rewards::DICTION_STRENGTH,
+            "+1 Precision Range" => Rewards::PRECISION_RANGE,
+            "+1 Calmness Strength" => Rewards::CALMNESS_STRENGTH,
+            "+1 Style Attempt on Style roll of 19 or 20" => Rewards::STYLE_EXPLODING,
+            _ => Rewards::NONE
+        })
     }
 }
