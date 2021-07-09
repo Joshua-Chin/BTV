8fd543f9b8032a93908f9f57265d74582aeca339
Joshua Chin <joshuarchin@gmail.com>
2021-07-08 20:25:24 -0400

    Implement the overall solve function.

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,13 @@ mod solver;
 
 fn main() {
     let challenges = parser::parse(include_str!("test_input.txt")).unwrap();
-    solver::solve(&challenges);
+    let solutions = solver::solve(&challenges);
+    for solution in solutions {
+        println!("Solution");
+        println!("Cost: {}", solution.cost);
+        println!("Success chance: {}", solution.log_proba.exp());
+        println!("Order: {:?}", solution.order);
+        println!("Abilities: {:?}", solution.abilities);
+        println!();
+    }
 }
--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -1,6 +1,6 @@
 use itertools::Itertools;
 
-use crate::{challenges::Challenge, rewards::Rewards};
+use crate::{abilities::Abilities, challenges::Challenge, rewards::Rewards};
 
 use self::{challenge::ChallengeSolver, merge::{MAX_TOTAL_COST, merge_hulls}};
 
@@ -10,9 +10,19 @@ mod merge;
 /// The number of cost values to considers. The total cost will be even.
 const TOTAL_COSTS: usize = (MAX_TOTAL_COST as usize) / 2 + 1;
 
-pub fn solve(challenges: &Vec<Challenge>) {
-    let challenge_solutions = ChallengeSolver::new(challenges);
+#[derive(Debug)]
+pub struct SolutionVertex {
+    pub cost: u32,
+    pub log_proba: f32,
+    pub order: Vec<usize>,
+    pub abilities: Vec<Abilities>,
+}
 
+pub fn solve(challenges: &Vec<Challenge>) -> Vec<SolutionVertex> {
+    // Precompute challenge setups
+    println!("Generating individual challenge setups");
+    let challenge_solutions = ChallengeSolver::new(challenges);
+    // Separate challenges with and without rewards
     let mut no_rewards = Vec::new();
     let mut rewards = Vec::new();
     for (idx, challenge) in challenges.iter().enumerate() {
@@ -21,22 +31,48 @@ pub fn solve(challenges: &Vec<Challenge>) {
             _ => rewards.push(idx),
         }
     }
-
-    for permutation in rewards.iter().permutations(rewards.len()) {
+    // Generate solution table
+    const INIT: Option<SolutionVertex> = None;
+    let mut table = [INIT; TOTAL_COSTS];
+    // Loop over each permutation of challenges.
+    println!("Searching for optimal challenge order");
+    let mut hulls = Vec::new();
+    hulls.reserve_exact(challenges.len());
+    for head in rewards.iter().permutations(rewards.len()) {
         let mut rewards = Rewards::NONE;
-        let mut hulls = Vec::new();
-        hulls.reserve_exact(challenges.len());
+        hulls.clear();
         // Sequence the challenges with no rewards after the other challenges.
+        let permutation: Vec<usize> = head.into_iter().chain(no_rewards.iter()).cloned().collect();
         // Collect the hulls.
-        for idx in permutation {
+        for idx in &permutation {
             hulls.push(challenge_solutions.solution_to(rewards, *idx));
             rewards |= challenges[*idx].reward;
         }
         // Merge the hulls.
         let merged_hull = merge_hulls(&hulls);
-
-        // TODO: Add hull to the table.
-
-        // TODO: Compute convex hull over table.
+        // Add hull to the table.
+        for candidate in merged_hull {
+            let idx = candidate.cost as usize / 2;
+            if let Some(vertex) = &table[idx] {
+                if vertex.log_proba > candidate.log_proba {
+                    break;
+                }
+            }
+            table[idx] = Some(SolutionVertex {
+                cost: candidate.cost,
+                log_proba: candidate.log_proba,
+                order: permutation.clone(),
+                abilities: candidate.configuration,
+            })
+        }
     }
+    // Gather the vertices from the table
+    let mut output = Vec::new();
+    for entry in table {
+        match entry {
+            Some(vertex) => output.push(vertex),
+            None => {},
+        }
+    }
+    output
 }
