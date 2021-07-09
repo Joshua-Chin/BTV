39f3b4019763c6eb228e1ae5180808963d65d76a
Joshua Chin <joshuarchin@gmail.com>
2021-07-08 22:38:41 -0400

    Add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,35 @@ mod parser;
 mod rewards;
 mod solver;
 
+use std::{fs::File, io::Read, path::Path};
+
+use clap::{AppSettings, Clap};
+
+#[derive(Clap)]
+#[clap(version="1.0", author="Joshua Chin")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Args {
+    /// The path of the input file.
+    input: String,
+}
+
 fn main() {
+    let args = Args::parse();
+
+    let input = {
+        let path = Path::new(&args.input);
+        let mut file = File::open(path).unwrap();
+        let mut input= String::new();
+        file.read_to_string(&mut input).unwrap();
+        input
+    };
+
     println!("Parsing challenges");
-    let challenges = parser::parse(include_str!("test_input.txt")).unwrap();
+    let challenges = parser::parse(input).unwrap();
+    
     let solutions = solver::solve(&challenges);
+    
     for solution in solutions {
-        println!("Solution");
         println!("Cost: {}", solution.cost);
         println!("Success chance: {}", solution.log_proba.exp());
         println!("Order: {:?}", solution.order);
